9e5c136f549b00b6f96d420dd8bdbc8d9d0e6b48
ANBU SEKAR M <SAIANBUSEKAR@GMAIL.COM>
2025-05-20 12:55:25 +0530

    Initial Commit

---
+++ shoppo/shoppoApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppoApp.swift
+//  shoppo
+//
+//  Created by Anbu Sekar M on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct shoppoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shoppoTests/shoppoTests.swift
@@ -0,0 +1,16 @@
+//
+//  shoppoTests.swift
+//  shoppoTests
+//
+//  Created by Anbu Sekar M on 20/05/25.
+//
+
+import Testing
+
+struct shoppoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
