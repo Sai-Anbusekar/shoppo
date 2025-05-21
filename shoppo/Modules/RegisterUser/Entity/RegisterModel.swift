d7d0ba3378d60831df5faafb8275d00f7d641551
ANBU SEKAR M <SAIANBUSEKAR@GMAIL.COM>
2025-05-21 21:25:56 +0530

    Registeration completed

---
+++ shoppo/Modules/RegisterUser/Entity/RegisterModel.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterProtocol.swift
+//  shoppo
+//
+//  Created by Anbu Sekar M on 21/05/25.
+//
+
+struct RegisterRequest: Codable {
+    let name: String
+    let email: String
+    let password: String
+    let dob: String
+    let phone: String
+}
+
+struct RegisterResponse: Codable {
+    let status: Bool
+    let message: String
+}
---
+++ shoppo/Modules/RegisterUser/Protocol/RegisterProtocol.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  shoppo
+//
+//  Created by Anbu Sekar M on 21/05/25.
+//
+
